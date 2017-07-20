c8943f29a4a1698acb7c3107bb002d7ea2ef5b7a
Zenchenko Alexander <zenchenko.alexander@gmail.com>
2017-07-20 16:26:26 +0300

    Form edited

--- src/AppBundle/Form/TaskForm.php
+++ src/AppBundle/Form/TaskForm.php
@@ -9,9 +9,29 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 class TaskForm extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
-			->add('name', null, array('label' => 'Задача:'))
-			->add('description', null, array('label' => 'Описание:'))
-			->add('executeData', DateType::class, array('label' => 'Дата:', 'widget' => 'single_text'))
-			->add('submit', SubmitType::class, array('label' => 'Создать'));
+			->add('name', null, array(
+					'label' => 'Задача:', 
+					'attr' => array(
+						'class' => 'form-control'
+			)))
+			->add('description', null, array(
+					'label' => 'Описание:',
+					'attr' => array(
+							'class' => 'form-control'
+					)		
+			))
+			->add('executeData', DateType::class, array(
+					'label' => 'Дата:',
+					'widget' => 'single_text',
+					'attr' => array(
+							'class' => 'form-control'
+					)		
+			))
+			->add('submit', SubmitType::class, array(
+					'label' => 'Создать',
+					'attr' => array(
+							'class' => 'btn'
+					)
+			));
 	}
 }
