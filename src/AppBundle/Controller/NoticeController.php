63de76b48c1b2b7f1cb8eaab66ddba76d8126d9a
Zenchenko Alexander <zenchenko.alexander@gmail.com>
2017-07-29 11:29:48 +0300

    Last updates

--- src/AppBundle/Controller/NoticeController.php
+++ src/AppBundle/Controller/NoticeController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -8,20 +7,32 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Notice;
 use AppBundle\Form\NoticeForm;
 
-class NoticeController extends Controller {
-        
-    public function showNoticesAction() {
-        ;
+class NoticeController extends Controller
+{
+
+    public function showNoticesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $qSQL = 'SELECT n FROM AppBundle:Notice n'; // Here will added condition to select by user id
+        $query = $em->createQuery($qSQL);
+        $notices = $query->getResult();
+        return $this->render('notice/shownotice.html.twig', array(
+            'notices' => $notices
+        ));
     }
-    
+
     /**
      * @Route("/add-notice", name="add-notice")
+     * 
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function addNoticeAction(Request $request) {
+    public function addNoticeAction(Request $request)
+    {
         $notice = new Notice();
-        $form = $this->createForm(NoticeForm::class, $notice);
+        $form = $this->createForm(NoticeForm::class, $notice, array(
+            'action' => $this->generateUrl('add-notice')
+        ));
         
         $form->handleRequest($request);
         
@@ -30,14 +41,34 @@ class NoticeController extends Controller {
             $em->persist($notice);
             $em->flush();
             
-            $this->redirectToRoute('homepage');
+            return $this->redirectToRoute('homepage');
         }
         
-        return $this->render('notice/addnotice.html.twig', array('form' => $form->createView()));
+        return $this->render('notice/addnotice.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
-    
-    public function delNoticeAction($id) {
-        ;
+
+    /**
+     * @Route("/del-notice/{id}", name="del-notice")
+     * 
+     * @param unknown $id
+     */
+    public function delNoticeAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $notice = $em->getRepository(Notice::class)->find($id);
+        
+        if (! $notice) {
+            throw $this->createNotFoundException('No task with id:' . $id);
+        }
+        
+        $em->remove($notice);
+        $em->flush();
+        
+        return $this->redirectToRoute('homepage');
     }
-    
+
+    public function editNoticeAction($id)
+    {}
 }
